8be7918e77fc212f707e148946a67f7e44eb376b
Sidhesh Goud <98638567+Sidhesh-Goud@users.noreply.github.com>
2023-04-13 22:55:39 +0530

    Update Throw.java

--- Throw.java
+++ Throw.java
@@ -1,6 +1,6 @@
 import java.util.*;
 class Age{
-    public static void eligible (int age){
+    public static void vote (int age){
         if(age<18)
             throw new ArithmeticException("You are not eligible");
         else
@@ -9,6 +9,6 @@ class Age{
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
-        eligible(sc.nextInt());
+        vote(sc.nextInt());
     }
 }
